20517f5b06d9ad56db43ca651c673374da26c664
Murad Ahmed <murad15-9149@diu.edu.bd>
2021-03-14 23:54:29 +0600

    basic comparison done

---
+++ src/Components/PiCalculation/PiCalculation.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PiCalculation = () => {
+    
+    return (
+        <div>
+            <h4>This is just calculation part</h4>
+        </div>
+    );
+};
+
+export default PiCalculation;
